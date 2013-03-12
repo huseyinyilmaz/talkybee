2fac0e0f7072c3bf57d501d63dcd7d4b40decd94
Huseyin Yilmaz <yilmazhuseyin@gmail.com>
2013-03-12 23:19:11 +0200

    Added chat.js

---
+++ www/js/chat.js
@@ -0,0 +1,13 @@
+window.chatApp =
+(function(){
+     var Comment = Backbone.Model.extend({});
+
+
+    
+    var MessageCollection = Backbone.Collection.extend({
+	model:Comment
+	});
+    alert(1);
+
+    
+})();
