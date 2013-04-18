aeeebdf7b2429e925665399858c70626d8316f1f
Huseyin Yilmaz <yilmazhuseyin@gmail.com>
2013-04-18 17:12:05 +0300

    Gets support for system configuration file. moved default port to 8765

--- lib/chat/src/c_utils.erl
+++ lib/chat/src/c_utils.erl
@@ -9,7 +9,7 @@
 -module(c_utils).
 
 %% API
--export([generate_code/0]).
+-export([generate_code/0, get_env/3, set_env/3]).
 
 %%%===================================================================
 %%% API
@@ -17,13 +17,34 @@
 
 %%--------------------------------------------------------------------
 %% @doc
+%% Generates random code as binary
 %% @end
 %%--------------------------------------------------------------------
-
 generate_code()->
     list_to_binary(
       integer_to_list(erlang:phash2({node(), now()}), 36)).
 
+%%--------------------------------------------------------------------
+%% @doc
+%% gets configuration for given app from sys.config file.
+%% if configuration does not exists, it uses given default value.
+%% @end
+%%--------------------------------------------------------------------
+-spec get_env(atom(), atom(), any()) -> any().
+get_env(AppName, Key, Default) ->
+    case application:get_env(AppName, Key) of
+	undefined -> Default;
+	{ok, Value} -> Value
+    end.
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Set environment variable for given app
+%% @end
+%%--------------------------------------------------------------------
+-spec set_env(atom(), atom(), any()) -> any().
+set_env(AppName, Key, Value) ->
+    application:set_env(AppName, Key, Value).
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
--- lib/http/src/http_app.erl
+++ lib/http/src/http_app.erl
@@ -11,10 +11,9 @@
 
 start(_StartType, _StartArgs) ->
     Dispatch = cowboy_router:compile(
-		 [{'_', [%% {"/bullet/", h_room_handler, []}
-			 
-			 %% },
-			 {"/deps/bullet/[...]", cowboy_static,
+		 [{c_utils:get_env(http, host, '_'), % host match
+		   [
+		         {"/deps/bullet/[...]", cowboy_static,
 			  [{directory, {priv_dir, bullet, []}},
 			   {mimetypes, {fun mimetypes:path_to_mimes/2, default}}]},
 
@@ -30,7 +29,10 @@ start(_StartType, _StartArgs) ->
 			   {mimetypes, {fun mimetypes:path_to_mimes/2, default}}]}
 			]}]),
     
-    {ok, _} = cowboy:start_http(http, 100, [{port, 8080}],
+    {ok, _} = cowboy:start_http(http, c_utils:get_env(http, pool_count, 100),
+				[{port, c_utils:get_env(http, port, 8765)},
+				 {max_connections,
+				  c_utils:get_env(http,max_connections,infinity)}],
 				[{env, [{dispatch, Dispatch}]}]).
 
 
